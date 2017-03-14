f2a950db1f7d1cb5d4205563f00754cc60caa5b1
Thanh Tran <trongthanh@gmail.com>
2017-03-14 14:11:33 +0700

    Step 1: graphql hello world

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * Step 1: Start simple GraphQL logic running inside NodeJS
+ */
+const { graphql, buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+	type Query {
+		hello: String
+	}
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+	hello() {
+		return 'Hello world!';
+	},
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+	console.log(response);
+});
